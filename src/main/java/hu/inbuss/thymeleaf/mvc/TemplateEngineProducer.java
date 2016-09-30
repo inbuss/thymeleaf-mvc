9df4f6ff735e56eb7d9a40b73958b0f34586f238
PÁLFALVI Tamás <tamas.palfalvi@inbuss.hu>
2016-09-30 13:53:40 +0200

    use the JAX-RS converter bridge if it is available

--- src/main/java/hu/inbuss/thymeleaf/mvc/TemplateEngineProducer.java
+++ src/main/java/hu/inbuss/thymeleaf/mvc/TemplateEngineProducer.java
@@ -1,26 +1,34 @@
 package hu.inbuss.thymeleaf.mvc;
 
-import java.util.Set;
+import hu.inbuss.thymeleaf.jaxrs.ParamConverterBridge;
 import javax.enterprise.context.Dependent;
+import javax.enterprise.context.spi.CreationalContext;
 import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
 import javax.inject.Inject;
 import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.templateresolver.ITemplateResolver;
 
 /**
  * @author PÁLFALVI Tamás &lt;tamas.palfalvi@inbuss.hu&gt;
  */
 @Dependent
 public class TemplateEngineProducer {
-    private final Set<ITemplateResolver> resolvers;
-
-    @Inject public TemplateEngineProducer(final TemplateResolvers resolvers) {
-        this.resolvers = resolvers.getTemplateResolvers();
-    }
+    @Inject private TemplateResolvers resolvers;
+    @Inject private BeanManager beanManager;
 
     @Produces public TemplateEngine getTemplateEngine() {
         TemplateEngine engine = new TemplateEngine();
-        engine.setTemplateResolvers(resolvers);
+        engine.setTemplateResolvers(resolvers.getTemplateResolvers());
+        try {
+            final Class<?> pcbClass = Class.forName("hu.inbuss.thymeleaf.jaxrs.ParamConverterBridge");
+            final Bean<?> pcbBean = beanManager.resolve(beanManager.getBeans(pcbClass));
+            final CreationalContext pcbContext = beanManager.createCreationalContext(pcbBean);
+            final ParamConverterBridge pcb = (ParamConverterBridge) beanManager.getReference(pcbBean, pcbClass, pcbContext);
+            pcb.installInto(engine);
+        } catch (final ClassNotFoundException cnfe) {
+            // the Jersey integration module is not present, do nothing
+        }
         return engine;
     }
 }
